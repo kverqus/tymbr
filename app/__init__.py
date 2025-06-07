de7b3760f1c80ad9cf35ee3e87db61dc57b8f832
Kristoffer Nilsson <kristoffer.nilsson@outlook.com>
2025-06-07 10:56:01 +0200

    feat: added logging capabilities

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from pathlib import Path
-
 from app.config import Config
+
+import os
+import logging
+from logging.handlers import RotatingFileHandler
 
 
 def create_app():
@@ -12,5 +15,22 @@ def create_app():
     # Ensure scripts directory exists
     scripts_dir = Path(app.config['SCRIPTS_DIRECTORY'])
     scripts_dir.mkdir(exist_ok=True)
+
+    # Ensure log directory exists
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+
+    file_handler = RotatingFileHandler(
+        'logs/tymbr.log',
+        maxBytes=10240000,
+        backupCount=10
+    )
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Tymbr startup')
 
     return app
